153d213f73352f76763cdbdf3bed62602e49de77
Yahya Dumba <127537884+YahyaDumba@users.noreply.github.com>
2025-06-01 13:12:28 +0530

    Create MaximumProductSubArray.java

---
+++ Arrays/MaximumProductSubArray.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int n=nums.length;
+        int  leftProduct=1;
+        int rightProduct=1;
+        int ans=nums[0];
+
+        for(int i=0;i<nums.length;i++){
+
+            leftProduct = (leftProduct == 0 || leftProduct < Integer.MIN_VALUE) ? 1 : leftProduct;
+            rightProduct = (rightProduct == 0 || rightProduct < Integer.MIN_VALUE) ? 1 : rightProduct;
+
+            leftProduct *= nums[i];
+            rightProduct *= nums[n-i-1];
+
+            ans=Math.max(ans,Math.max(leftProduct,rightProduct));
+        }
+        return (int) ans;
+    }
+}
