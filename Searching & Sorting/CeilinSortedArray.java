daaceb7baa6e7cc903de33f6dfa1dbd0ad3a9687
Yahya Dumba <127537884+YahyaDumba@users.noreply.github.com>
2025-06-24 18:50:03 +0530

    Create CeilinSortedArray.java

---
+++ Searching & Sorting/CeilinSortedArray.java
@@ -0,0 +1,21 @@
+// User function Template for Java
+class Solution {
+    public int findCeil(int[] arr, int x) {
+        // code here
+       int low = 0;
+       int high =arr.length-1;
+       int result  = -1;
+       while(low<=high){
+           int mid = low +(high -low) / 2;
+       
+       if(arr[mid]>=x){
+           result = mid;    
+           high = mid-1;
+       }
+       else{
+           low = mid+1;
+       }
+       }
+       return result;
+    }
+}
