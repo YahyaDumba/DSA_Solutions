ee2b423b4d913e0e3e3acf1f045431a986c3c5a7
Yahya Dumba <127537884+YahyaDumba@users.noreply.github.com>
2025-07-05 21:56:15 +0530

    Update SubsetSum1.java

--- Recursion/SubsetSum1.java
+++ Recursion/SubsetSum1.java
@@ -1,18 +1,40 @@
-// User function Template for Java//User function Template for Java
-class Solution {
-    public static void func(int i,int sum, ArrayList<Integer> arr,int N, ArrayList<Integer> ans){
-        if(i==N){
-            ans.add(sum);
+import java.util.*;
+class TUF {
+    static void subsetSumsHelper(int ind, int sum, ArrayList < Integer > arr, int N, ArrayList < Integer > sumSubset) {
+        if (ind == N) {
+            sumSubset.add(sum);
             return;
         }
-            func(i+1,sum+arr.get(i),arr,N,ans);
-            func(i+1,sum,arr,N,ans);
+
+        // pick the element 
+        subsetSumsHelper(ind + 1, sum + arr.get(ind), arr, N, sumSubset);
+
+        // Do-not pick the element
+        subsetSumsHelper(ind + 1, sum, arr, N, sumSubset);
     }
-    public ArrayList<Integer> subsetSums(int[] arr) {
-        // code here
-        ArrayList<Integer> ans = new ArrayList<>();
-        func(0,0,arr,N,ans);
+
+    static ArrayList < Integer > subsetSums(ArrayList < Integer > arr, int N) {
+
+        ArrayList < Integer > sumSubset = new ArrayList < > ();
+        subsetSumsHelper(0, 0, arr, N, sumSubset);
+        Collections.sort(sumSubset);
+        return sumSubset;
+    }
+
+
+
+    public static void main(String args[]) {
+
+
+        ArrayList < Integer > arr = new ArrayList < > ();
+        arr.add(3);
+        arr.add(1);
+        arr.add(2);
+        ArrayList < Integer > ans = subsetSums(arr, arr.size());
         Collections.sort(ans);
-        return ans;
+        System.out.println("The sum of each subset is ");
+        for (int i = 0; i < ans.size(); i++)
+            System.out.print(ans.get(i) + " ");
     }
+
 }
